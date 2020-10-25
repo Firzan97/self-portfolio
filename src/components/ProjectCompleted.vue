8b9b08ede9c7ac309da31f9f5574c55c1a837ca0
Firzan97 <FirzanAzrai97@gmail.com>
2020-10-25 20:39:23 +0800

    change public path in production2

--- src/components/ProjectCompleted.vue
+++ src/components/ProjectCompleted.vue
@@ -30,14 +30,14 @@ export default {
     return {
       projects: [
         {
-          image: "@/assets/calculator.PNG",
+          image: "public/assets/calculator.PNG",
           name: "Calculator Application",
           description:
             "Calculator application created using Vuejs. Developing the app for the purpose of learning on how vue js functioning.",
           link: "https://master.d131ilqmg8wmfc.amplifyapp.com/",
         },
         {
-          image: "/assets/currencyconverter.PNG",
+          image: "assets/currencyconverter.PNG",
           name: "Currency Converter",
           description:
             "A simple currency converter application created using vuejs. The data retrieve from Exchangerateapi.io API and the also from restcountries API. The APIs are pulled using axios",
